420bd402b8b3971827c708d57929c3d10d79b283
asierbrizu <asierbrizu@opendeusto.es>
2021-11-14 19:39:08 +0100

    TopVideo v1.0

--- appTopVideo/urls.py
+++ appTopVideo/urls.py
@@ -3,12 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('video/<str:idVideo>', views.video, name="video"),
+    path('video/<int:idVideo>', views.video, name="video"),
     path('videos/', views.videos, name="videos"),
-    path('plataforma/<str:nombrePlataforma>', views.plataforma, name="plataforma"),
+    path('plataforma/<int:idPlataforma>', views.plataforma, name="plataforma"),
     path('plataformas/', views.plataformas, name="plataformas"),
-    path('categoria/<str:nombreCategoria>', views.categoria, name="categoria"),
+    path('categoria/<int:idCategoria>', views.categoria, name="categoria"),
     path('categorias/', views.categorias, name="categorias"),
-  
-    path('prueba/', views.prueba, name="prueba"),
     ]
--- appTopVideo/views.py
+++ appTopVideo/views.py
@@ -19,26 +19,60 @@ def categorias(request):
 
 #Devuelve un listado de videos
 def videos(request):
-  videos= get_list_or_404(Video.objects.order_by('identificador'))
+  videos= get_list_or_404(Video.objects.order_by('fecha'))
   context = {'lista_videos': videos }
   return render(request, 'videos.html', context)
 
-def plataforma(request, nombrePlataforma):
-  plataforma= get_object_or_404(Plataforma.pk==nombrePlataforma)
-  context = {'plataforma': plataforma }
+def plataforma(request, idPlataforma):
+  plataforma= get_object_or_404(Plataforma, pk=idPlataforma)
+  seleccionados=[]
+  videos=get_list_or_404(Video)
+  for v in videos:
+    plataformas=v.plataformas.all()
+    for p in plataformas:
+        if p.nombre == plataforma.nombre:
+            seleccionados.append(v)
+  
+  
+  context = {'plataforma': plataforma, 'videos': seleccionados}
   return render(request, 'plataforma.html', context)
 
-def categoria(request, nombreCategoria):
-  categoria= get_object_or_404(Categoria.pk==nombreCategoria)
-  context = {'categoria': categoria }
+def categoria(request, idCategoria):
+  categoria= get_object_or_404(Categoria, pk=idCategoria)
+  seleccionados=[]
+  videos=get_list_or_404(Video)
+  for v in videos:
+    if v.categoria.nombre == categoria.nombre:
+            seleccionados.append(v)
+  
+  context = {'categoria': categoria, 'videos':seleccionados }
   return render(request, 'categoria.html', context)
 
 def video(request, idVideo):
-  video= get_object_or_404(Video.pk==idVideo)
-  context = {'video': video }
+  video= get_object_or_404(Video, pk=idVideo)
+  plataformasVideo=video.plataformas.all()
+  seleccionados=[]
+  plataformas=get_list_or_404(Plataforma)
+  for p in plataformas:
+    for p2 in plataformasVideo:
+        if p == p2:
+            seleccionados.append(p)
+  context = {'video': video, 'plataformas': seleccionados}
   return render(request, 'video.html', context)
 
 def index(request):
-  return render(request, 'index.html')
-def prueba(request):
-  return render(request, 'base.html')
+  categorias=get_list_or_404(Categoria)
+  todosVideos= get_list_or_404(Video.objects.order_by('fecha'))
+  videos=[]
+  
+  for c in categorias:
+    actual="1970-01-01"
+    video=None
+    for v in todosVideos:
+        if v.categoria.nombre==c.nombre and v.fecha.isoformat()>actual:
+            actual=v.fecha.isoformat()
+            video=v
+    if video!=None:
+        videos.append(video)        
+  context = {'videos': videos}
+  return render(request, 'index.html', context)
